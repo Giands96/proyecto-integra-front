e96e4e9852d74053b581dd558a609020e4732587
Axel <dsg4857@gmail.com>
2026-04-09 21:27:31 -0500

    feat: corregir nombre del campo 'disponibilidad' a 'disponible' en la interfaz Camion

--- src/modules/camiones/types.ts
+++ src/modules/camiones/types.ts
@@ -3,7 +3,7 @@ import { PaginatedResponse } from "@/types/pagination";
 export interface Camion {
   idCamion?: number;
   placa: string;
-  disponibilidad: number;
+  disponible: number;
 }
 
 export type CamionPaginatedResponse = PaginatedResponse<Camion>;
