02e31dfffa6eb64ef49a89811fbb1574fffc7907
Axel <dsg4857@gmail.com>
2026-04-09 21:27:40 -0500

    feat: cambiar el campo 'disponibilidad' a 'disponible' y modificar su tipo a booleano en el esquema de camiones

--- src/modules/camiones/schemas.ts
+++ src/modules/camiones/schemas.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const camionSchema = z.object({
   placa: z.string().min(1, "Placa es requerida").max(15, "Maximo 15 caracteres"),
-  disponibilidad: z.number().int().min(0).max(1),
+  disponible: z.boolean(),
 });
